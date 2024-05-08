30c2a20ba9ac685dd1d60bd5737a7fdc1981c6ed
MH020 <166133291+MH020@users.noreply.github.com>
2024-05-08 13:01:27 +0200

    result fixed

--- Result.java
+++ Result.java
@@ -1,19 +1,14 @@
 import java.time.LocalDateTime;
 
-<<<<<<<< HEAD:Result.java
 public class Result{
-========
-public class CompResult{
->>>>>>>> 17c713466586a8e4f803098173d01148283e5e2c:CompResult.java
 
    //Atributer
    int length; 
    String swimmingStyle; 
    int time;
-   LocalDateTime Date;
-   String location;  
+   LocalDateTime Date;  
    
-   public Result(int length, String swimmingStyle, int time, LocalDateTime date, String location) {
+   public Result(int length, String swimmingStyle, int time, LocalDateTime date) {
     this.length = length;
     this.swimmingStyle = swimmingStyle;
     this.time = time;
