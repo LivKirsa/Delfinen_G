d038d9de981a96b74920d55eb59cf3788622a7de
Liv Kirsa <166127262+LivKirsa@users.noreply.github.com>
2024-05-24 13:04:53 +0200

    Rename ChairMan

--- SwingCoach.java
+++ SwingCoach.java
@@ -3,8 +3,20 @@ import java.awt.*;
 import java.awt.event.*;
 import java.util.ArrayList;
 import javax.swing.table.DefaultTableModel;
+import java.time.LocalDate;
 
 public class SwingCoach extends JPanel {
+   Color turkis1 = new Color(199, 237, 231);
+   Color turkis2 = new Color(177, 224, 218);
+   Color turkis3 = new Color(140, 204, 200);
+   Color turkis4 = new Color(103, 159, 157);
+   Color purple = new Color(159, 141, 185);
+   Color pink = new Color(232, 173, 210);
+   Color blue = new Color(171, 218, 233);
+   Color softWhite = new Color(241, 234, 231);
+   Color darkBlue = new Color(53, 120, 180);
+   Color softBlack = new Color(50, 42, 51);
+   
    JFrame f = new JFrame(); 
    
     public SwingCoach(boolean visible) {
@@ -21,14 +33,18 @@ public class SwingCoach extends JPanel {
       
       public void buttonPanel(){
         JPanel buttonPanel = new JPanel(new GridLayout(30,1)); 
-        JButton b1 = new JButton("Se Holdlister");
-        JButton b2 = new JButton("Indtast svømmeresultater"); 
-        JButton b3 = new JButton("Holdoversigt");
-        b1.setBackground(Color.BLUE);
+        JButton b1 = new JButton("Se Holdmedlemmer");
+        JButton b2 = new JButton("Nyt Resultat"); 
+        JButton b3 = new JButton("Se alle Hold");
+        
+        b1.setBackground(pink);
+        b1.setForeground(softBlack);
+        b2.setBackground(blue);
+        b2.setForeground(softBlack);
+        b3.setBackground(pink);
+        b3.setForeground(softBlack);
         buttonPanel.add(b1);
-        b2.setBackground(Color.RED);
         buttonPanel.add(b2);
-        b3.setBackground(Color.GREEN);
         buttonPanel.add(b3);
         
  //add Buttonpanel til WEST
@@ -42,7 +58,7 @@ public class SwingCoach extends JPanel {
       //actions of the se medlem button 
       b2.addActionListener(new ActionListener() {
          public void actionPerformed(ActionEvent e) {
-         //SwimmerFrame();
+         resultFrame();
          }
        });
        //actions of the add trainer to a team. 
@@ -179,4 +195,131 @@ public class SwingCoach extends JPanel {
 
       return tableModel;
     }  
+    
+     public void resultFrame() {
+        JFrame resultFrame = new JFrame();
+        resultFrame.setVisible(true);
+        resultFrame.setSize(300, 300);
+        resultFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        Login.centerFrameOnScreen(resultFrame);
+
+        JPanel panel = new JPanel(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.insets = new Insets(5, 5, 5, 5);
+        // JPanel panel = new JPanel();
+        panel.setBackground(blue);
+
+        JTextField text1Field = new JTextField(20);
+        JTextField text2Field = new JTextField(20);
+        JLabel label1 = new JLabel("Name:");
+        JLabel label2 = new JLabel("Birthday:");
+
+        JCheckBox checkBox1 = new JCheckBox("isActiveMembership");
+        JCheckBox checkBox2 = new JCheckBox("isCompetitiveSwimmer");
+        JCheckBox checkBox3 = new JCheckBox("isMan");
+         
+        //this just places it around found online. 
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.anchor = GridBagConstraints.WEST;
+        panel.add(label1, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        gbc.anchor = GridBagConstraints.WEST;
+        panel.add(text1Field, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        gbc.anchor = GridBagConstraints.WEST;
+        panel.add(label2, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 3;
+        gbc.anchor = GridBagConstraints.WEST;
+        panel.add(text2Field, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 4;
+        gbc.anchor = GridBagConstraints.WEST;
+        panel.add(checkBox1, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 5;
+        gbc.anchor = GridBagConstraints.WEST;
+        panel.add(checkBox2, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 6;
+        gbc.anchor = GridBagConstraints.WEST;
+        panel.add(checkBox3, gbc);
+        
+         // Adding the Done button
+        JButton doneButton = new JButton("Done");
+        gbc.gridx = 0;
+        gbc.gridy = 7;
+        gbc.anchor = GridBagConstraints.CENTER;
+        panel.add(doneButton, gbc);
+
+        resultFrame.add(panel);
+
+        checkBox1.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (checkBox1.isSelected()) {
+                    System.out.println("Box 1 was checked");
+                } else {
+                    System.out.println("Box 1 was unchecked");
+                }
+            }
+        });
+
+        checkBox2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (checkBox2.isSelected()) {
+                    System.out.println("Box 2 was checked");
+                } else {
+                    System.out.println("Box 2 was unchecked");
+                }
+            }
+        });
+
+        checkBox3.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                if (checkBox3.isSelected()) {
+                    System.out.println("Box 3 was checked");
+                } else {
+                    System.out.println("Box 3 was unchecked");
+                }
+            }
+        });
+            doneButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            String name = text1Field.getText(); 
+            String birthdayString = text2Field.getText(); 
+            boolean isActiveMembership = checkBox1.isSelected();
+            boolean isCompetitiveSwimmer = checkBox2.isSelected();
+            boolean isMan = checkBox3.isSelected();
+            
+            LocalDate birthday;
+            try {
+               birthday = LocalDate.parse(birthdayString);
+            } catch (Exception ex) {
+               JOptionPane.showMessageDialog(resultFrame, "Invalid birthday format. Please use YYYY-MM-DD.");
+            return;
+            }
+            Member newMember = new Member(name, birthdayString, isActiveMembership, isCompetitiveSwimmer, isMan);
+            resultFrame.dispose(); 
+            }
+        }); 
+            text1Field.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+            text2Field.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            }
+        });
+    } // This ends swimmerFrame
+
+
 }
