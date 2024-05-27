9aeda6b1db992052b9622ddb00567f3997ec6092
Liv Kirsa <166127262+LivKirsa@users.noreply.github.com>
2024-05-27 23:45:10 +0200

    Multiple choice for the 3 booleans in New Member

--- SwingChairMan.java
+++ SwingChairMan.java
@@ -17,185 +17,173 @@ public class SwingChairMan extends SuperSwing {
       super (visible);
       f.setTitle("Svømmeklubben Delfinen");
       
-        //Tabs work start here 
+        //Tabs work starts here 
         JTabbedPane tabbedPane = new JTabbedPane();
         tabbedPane.addTab("Medlemmer",openChairman());
-        tabbedPane.addTab("Hold", openCoach());//openCoach());
+        tabbedPane.addTab("Hold", openCoach());
         tabbedPane.addTab("Kontingent",openAcc());
         f.add(tabbedPane, BorderLayout.CENTER);
-        
-        
         f.setLocationRelativeTo(null);
         f.setVisible(true);
         setBackground(turkis1);
-        //setLayout(new BorderLayout());
-        }
+   }
+   
         protected JButton Button1() {
         JButton b1 = new JButton("Nyt medlem");
-                        b1.setBackground(pink);
-                        b1.setForeground(softBlack);
-        b1.addActionListener(new ActionListener() {
-        //action for button 1
-            public void actionPerformed(ActionEvent e) {
-                            swimmerFrame();
-
-            }
-        });
-        return b1;
-    }
+           b1.setBackground(pink);
+           b1.setForeground(softBlack);
+           b1.addActionListener(new ActionListener() {
+               public void actionPerformed(ActionEvent e) {
+                  swimmerFrame();
+               }
+           });
+           return b1;
+        }
+        
         protected JButton Button2() {
         JButton b2 = new JButton("Se Medlemmer");
-                b2.setBackground(blue);
-        b2.setForeground(softBlack);
-        b2.addActionListener(new ActionListener() {
-        //action for button 2
-            public void actionPerformed(ActionEvent e) {
-                        displayList(addTableModel(MemberList.memberList, Member.col));
-
-            }
-        });
-        return b2;
-    }/*
-        protected JButton Button3() {
-        JButton b3 = new JButton("Se Hold");
-                b3.setBackground(pink);
-        b3.setForeground(softBlack);
-        b3.addActionListener(new ActionListener() {
-        //action for button 3
-            public void actionPerformed(ActionEvent e) {
-                           displayList(addTableModel(TeamList.teamList, Team.col));
-            }
-        });
-        return b3;
-    }*/
-
- 
+           b2.setBackground(blue);
+           b2.setForeground(softBlack);
+           b2.addActionListener(new ActionListener() {
+               public void actionPerformed(ActionEvent e) {
+                  displayList(addTableModel(MemberList.memberList, Member.col));
+               }
+           });
+           return b2;
+        }
     
         private JPanel openChairman() {
-        JPanel panel = new JPanel(new BorderLayout());
-        panel.setBackground(new Color(255, 200, 200));
-        //panel.add(chairManButtons(),BorderLayout.WEST);
-         panel.add(this); //BorderLayout.CENTER);
-        return panel;
+           JPanel panel = new JPanel(new BorderLayout());
+           panel.setBackground(turkis2);
+           //panel.add(chairManButtons(),BorderLayout.WEST);
+            panel.add(this); //BorderLayout.CENTER);
+           return panel;
         } 
+        
         private JPanel openCoach() {
            JPanel panel = new JPanel(new BorderLayout());
-           panel.setBackground(new Color(255, 200, 200));
+           panel.setBackground(turkis2);
            panel.add(new SwingCoach(false));
            return panel;
         }
         
         private JPanel openAcc() {
         JPanel panel = new JPanel(new BorderLayout());
-           panel.setBackground(new Color(255, 200, 200));
+           panel.setBackground(turkis2);
            panel.add(new SwingAcc(false));
- 
-        return panel;
+           return panel;
         }
             
- public void swimmerFrame() {
-        JFrame swimmerFrame = new JFrame();
-        swimmerFrame.setVisible(true);
-        swimmerFrame.setSize(300, 300);
-        swimmerFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        Login.centerFrameOnScreen(swimmerFrame);
+   public void swimmerFrame() {
+   
+     // Method for adding new Member
+     JFrame swimmerFrame = new JFrame();
+     swimmerFrame.setTitle("Opret nyt Medlem");
+     swimmerFrame.setVisible(true);
+     swimmerFrame.setSize(300, 300);
+     swimmerFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+     Login.centerFrameOnScreen(swimmerFrame);
+   
+     JPanel panel = new JPanel(new GridBagLayout());
+     GridBagConstraints gbc = new GridBagConstraints();
+     gbc.insets = new Insets(5, 5, 5, 5);
+     panel.setBackground(blue);
+   
+     JTextField text1Field = new JTextField(20);
+     JTextField text2Field = new JTextField(20);
+     JLabel label1 = new JLabel("Navn: ");
+     JLabel label2 = new JLabel("Fødselsdag: ");
+     
+     ButtonGroup genderGroup = new ButtonGroup();
+     ButtonGroup membershipGroup = new ButtonGroup();
+     ButtonGroup competitionGroup = new ButtonGroup();
+   
+     JRadioButton maleRadioButton = new JRadioButton("Mand");
+     JRadioButton femaleRadioButton = new JRadioButton("Kvinde");
+     maleRadioButton.setBackground(blue);
+     femaleRadioButton.setBackground(blue);
+     genderGroup.add(maleRadioButton);
+     genderGroup.add(femaleRadioButton);
+   
+     JRadioButton activeRadioButton = new JRadioButton("Aktiv");
+     JRadioButton passiveRadioButton = new JRadioButton("Passiv");
+     activeRadioButton.setBackground(blue);
+     passiveRadioButton.setBackground(blue);
+     membershipGroup.add(activeRadioButton);
+     membershipGroup.add(passiveRadioButton);
+   
+     JRadioButton competitiveRadioButton = new JRadioButton("Konkurrence");
+     JRadioButton casualRadioButton = new JRadioButton("Motionist");
+     competitiveRadioButton.setBackground(blue);
+     casualRadioButton.setBackground(blue);
+     competitionGroup.add(competitiveRadioButton);
+     competitionGroup.add(casualRadioButton);
+   
+     gbc.gridx = 0;
+     gbc.gridy = 0;
+     gbc.anchor = GridBagConstraints.CENTER;
+     panel.add(label1, gbc);
+   
+     gbc.gridx = 0;
+     gbc.gridy = 1;
+     gbc.anchor = GridBagConstraints.CENTER;
+     panel.add(text1Field, gbc);
+   
+     gbc.gridx = 0;
+     gbc.gridy = 2;
+     gbc.anchor = GridBagConstraints.CENTER;
+     panel.add(label2, gbc);
+   
+     gbc.gridx = 0;
+     gbc.gridy = 3;
+     gbc.anchor = GridBagConstraints.CENTER;
+     panel.add(text2Field, gbc);
 
-        JPanel panel = new JPanel(new GridBagLayout());
-        GridBagConstraints gbc = new GridBagConstraints();
-        gbc.insets = new Insets(5, 5, 5, 5);
-        // JPanel panel = new JPanel();
-        panel.setBackground(blue);
+     gbc.gridx = 0;
+     gbc.gridy = 4;
+     gbc.anchor = GridBagConstraints.WEST;
+     panel.add(maleRadioButton, gbc);
+   
+     gbc.gridx = 0;
+     gbc.gridy = 4;
+     gbc.anchor = GridBagConstraints.EAST;
+     panel.add(femaleRadioButton, gbc);
 
-        JTextField text1Field = new JTextField(20);
-        JTextField text2Field = new JTextField(20);
-        JLabel label1 = new JLabel("Name:");
-        JLabel label2 = new JLabel("Birthday:");
+     gbc.gridx = 0;
+     gbc.gridy = 5;
+     gbc.anchor = GridBagConstraints.WEST;
+     panel.add(activeRadioButton, gbc);
+   
+     gbc.gridx = 0;
+     gbc.gridy = 5;
+     gbc.anchor = GridBagConstraints.EAST;
+     panel.add(passiveRadioButton, gbc);
 
-        JCheckBox checkBox1 = new JCheckBox("isActiveMembership");
-        JCheckBox checkBox2 = new JCheckBox("isCompetitiveSwimmer");
-        JCheckBox checkBox3 = new JCheckBox("isMan");
-         
-        //this just places it around found online. 
-        gbc.gridx = 0;
-        gbc.gridy = 0;
-        gbc.anchor = GridBagConstraints.WEST;
-        panel.add(label1, gbc);
-
-        gbc.gridx = 0;
-        gbc.gridy = 1;
-        gbc.anchor = GridBagConstraints.WEST;
-        panel.add(text1Field, gbc);
-
-        gbc.gridx = 0;
-        gbc.gridy = 2;
-        gbc.anchor = GridBagConstraints.WEST;
-        panel.add(label2, gbc);
-
-        gbc.gridx = 0;
-        gbc.gridy = 3;
-        gbc.anchor = GridBagConstraints.WEST;
-        panel.add(text2Field, gbc);
-
-        gbc.gridx = 0;
-        gbc.gridy = 4;
-        gbc.anchor = GridBagConstraints.WEST;
-        panel.add(checkBox1, gbc);
-
-        gbc.gridx = 0;
-        gbc.gridy = 5;
-        gbc.anchor = GridBagConstraints.WEST;
-        panel.add(checkBox2, gbc);
-
-        gbc.gridx = 0;
-        gbc.gridy = 6;
-        gbc.anchor = GridBagConstraints.WEST;
-        panel.add(checkBox3, gbc);
-        
-         // Adding the Done button
-        JButton doneButton = new JButton("Done");
-        gbc.gridx = 0;
-        gbc.gridy = 7;
-        gbc.anchor = GridBagConstraints.CENTER;
-        panel.add(doneButton, gbc);
-
-        swimmerFrame.add(panel);
-
-        checkBox1.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (checkBox1.isSelected()) {
-                    System.out.println("Box 1 was checked");
-                } else {
-                    System.out.println("Box 1 was unchecked");
-                }
-            }
-        });
-
-        checkBox2.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (checkBox2.isSelected()) {
-                    System.out.println("Box 2 was checked");
-                } else {
-                    System.out.println("Box 2 was unchecked");
-                }
-            }
-        });
-
-        checkBox3.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (checkBox3.isSelected()) {
-                    System.out.println("Box 3 was checked");
-                } else {
-                    System.out.println("Box 3 was unchecked");
-                }
-            }
-        });
+     gbc.gridx = 0;
+     gbc.gridy = 6;
+     gbc.anchor = GridBagConstraints.WEST;
+     panel.add(competitiveRadioButton, gbc);
+   
+     gbc.gridx = 0;
+     gbc.gridy = 6;
+     gbc.anchor = GridBagConstraints.EAST;
+     panel.add(casualRadioButton, gbc);
+   
+     // Done button
+     JButton doneButton = new JButton("Tilføj");
+     gbc.gridx = 0;
+     gbc.gridy = 7;
+     gbc.anchor = GridBagConstraints.CENTER;
+     panel.add(doneButton, gbc);
+   
+     swimmerFrame.add(panel);
             doneButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
             String name = text1Field.getText(); 
             String birthdayString = text2Field.getText(); 
-            boolean isActiveMembership = checkBox1.isSelected();
-            boolean isCompetitiveSwimmer = checkBox2.isSelected();
-            boolean isMan = checkBox3.isSelected();
+            boolean isActiveMembership = activeRadioButton.isSelected();
+            boolean isCompetitiveSwimmer = competitiveRadioButton.isSelected();
+            boolean isMan = maleRadioButton.isSelected();
             
             LocalDate birthday;
             try {
