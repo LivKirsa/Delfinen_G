e2aaa5952aaa60c2658061d494dae6a02cdb9c45
signelystrup <166133407+signelystrup@users.noreply.github.com>
2024-05-28 14:24:11 +0200

    knapper opdateret

--- SwingTeam.java
+++ SwingTeam.java
@@ -7,14 +7,30 @@ public class SwingTeam extends SuperSwing{//class to be added to Couch class.
    public SwingTeam(int teamNumber){
       super(false);
       setVisible(true);
-      setButtons();
-      super.Button1().setText("Se discipliner");
-      b2.setText("Se Medlemmer");
+      //setButtons();
+     // super.Button1().setText("Se discipliner");
+      //b2.setText("Se Medlemmer");
    }
    
+   /*
    public void setButtons(){
       b1 = super.Button1();
       b2 = super.Button2();
+   }*/
+   
+   public JButton Button1(){
+      JButton button = super.Button1();
+      button.setText("Se discipliner");
+      button.setBackground(turkis1);
+      return button;
+   }
+   
+   public JButton Button2(){
+   JButton button = super.Button2();
+   button.setText("Se Medlemmer");
+   button.setBackground(blue);
+      return button;
+
    }
    
    
